dd0498ac48a5be7301c49ea8a0ed72ee992c4d77
Yead <yead19@gmail.com>
2025-02-23 22:41:24 +0600

    comment connect

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const userCollection = client.db('ToDo-DB').collection('users')
         const taskCollection = client.db('ToDo-DB').collection('tasks')
@@ -184,8 +184,8 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
 
     } finally {
